c9f2ac0bf4389a8e88f6be46259f9b5e4db9ee04
yegaoteng <2287681747@qq.com>
2026-06-18 18:17:25 +0800

    Add files via upload

--- worker.js
+++ worker.js
@@ -14,7 +14,7 @@ export default {
     const pg = parseInt(url.searchParams.get("pg") || "1");
 
     // 从 S3 拉你现有的 latest.json
-    const S3_JSON = "https://s3.hi168.com/hi168-25242-ttcarm4a-s/movies/latest.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=0WE7Y5ZRQJPHN2GCKDC5%2F20260618%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20260618T100754Z&X-Amz-Expires=3600&X-Amz-Signature=37a7349c823c5de9ed46bd1906af762439084d5d3740e1b426ae6b81726a17ab&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D%22latest.json%22&response-content-type=application%2Fjson&x-id=GetObject";
+    const S3_JSON = "https://s3.hi168.com/hi168-25242-ttcarm4a-s/movies/latest.json?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=0WE7Y5ZRQJPHN2GCKDC5%2F20260618%2Fauto%2Fs3%2Faws4_request&X-Amz-Date=20260618T101652Z&X-Amz-Expires=3600&X-Amz-Signature=3c0a10bc0fd9389960350083673942e54bd46b53d33dfcdf9d53596bb8faf5e3&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D%22latest.json%22&response-content-type=application%2Fjson&x-id=GetObject";
     let raw;
     try {
       raw = await fetch(S3_JSON, { cf: { cacheEverything: true, cacheTtl: 300 } });
