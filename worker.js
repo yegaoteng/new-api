7c899d9afdf986eb68746dd200ff2b151a50c767
yegaoteng <2287681747@qq.com>
2026-06-20 08:50:39 +0800

    Add files via upload

--- worker.js
+++ worker.js
@@ -5,11 +5,11 @@ export default {
     // /files/a/b.jpg → OpenList /d/hi168/a/b.jpg
     // 如果你的 OpenList 存储名不是 hi168，改成对应存储名
     if (url.pathname.startsWith("/files/")) {
-      url.pathname = "/d/s3/" + url.pathname.slice("/files/".length);
+      url.pathname = "/d/hi168/" + url.pathname.slice("/files/".length);
     }
 
     // ★ 换成你的 Sealos OpenList 域名（不要加结尾斜杠）
-    url.hostname = "fmptdghi.sealosbja.site";
+    url.hostname = "rmbtqegr.sealosgzg.site";
 
     // 保留原始请求（Header / Method / Range）
     const newReq = new Request(url.toString(), {
